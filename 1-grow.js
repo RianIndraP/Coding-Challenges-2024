03f968400128af3cfc8bcb4bba6b1ae60dc9aafe
RianIndraP <www.rian2008@gmail.com>
2024-08-23 18:51:58 +0700

    Day 1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,19 +1,15 @@
-// WPU
-
-// function grow (x){
-//     let result = x[0]
-//     for(let i = 1; i < x.length; i++){
-
-//         result = result * x[i];
-//     }
-//         return result;
-// }
+// WPU Coding Challenge 2024
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
 
 // function grow(x) {
-//     const result = x.reduce((acc, curr) => acc * curr, 1);
-//     return result;
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result *= x[i];
+//   }
+//   return result;
 // }
 
-const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
+const grow = (x) => x.reduce((acc, curr) => acc * curr);
 
-console.log(grow([1, 2, 3, 4]));
+console.log(grow([2, 2, 2, 2, 2, 2]));
