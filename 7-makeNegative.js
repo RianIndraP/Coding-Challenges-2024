0cc638c172dc02fb135c5426c9257a159c703a0a
RianIndraP <www.rian2008@gmail.com>
2024-08-24 21:05:29 +0700

    Day 7/366

--- 7-makeNegative.js
+++ 7-makeNegative.js
@@ -2,7 +2,7 @@
 // 7/366
 // https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 
-// // 1. Function Declaration
+// 1. Function Declaration
 //   function makeNegative(num) {
 //     return num < 0 ? num : -num;
 //   }
