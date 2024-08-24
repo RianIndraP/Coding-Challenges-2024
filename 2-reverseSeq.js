01e208534ced2adb8b30b04df6503e371ad4cfab
RianIndraP <www.rian2008@gmail.com>
2024-08-24 11:42:20 +0700

    Day 3/366

--- 2-reverseSeq.js
+++ 2-reverseSeq.js
@@ -1,3 +1,7 @@
+// WPU Coding Challenge 2024
+// 2/366
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
 // 1. Name Function Declaration
     // function reverseSeq(n) {
     //     let arr = [];
