db7589be1c535ea1c2f702886d65e6b66f78b967
RianIndraP <www.rian2008@gmail.com>
2024-08-25 11:34:36 +0700

    Day 10/366

---
+++ 10-numberToString.js
@@ -0,0 +1,21 @@
+// WPU Challenge 2024
+// 10/366
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+// 1. Function Declaration
+  // function numberToString(num) {
+  //   return num.toString()
+  // }
+
+// 2. Function Expression
+  // const numberToString = function(num) {
+  //   return num.toString()
+  // }
+
+// 3. Arrow Function
+  // const numberToString = num => num.toString()
+  
+// 4. Cara Lain
+  numberToString = String
+
+console.log(numberToString(67));
