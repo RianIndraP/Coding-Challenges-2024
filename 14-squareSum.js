93332a5c7133396ca5aaf26550b43dabbc8ef00d
RianIndraP <www.rian2008@gmail.com>
2024-08-25 12:45:02 +0700

    Day 14/366

---
+++ 14-squareSum.js
@@ -0,0 +1,27 @@
+// WPU Challenge 2024
+// 13/366
+// https://www.codewars.com/kata/515e271a311df0350d00000f/train/javascript
+
+// 1. Function Declaration
+    // function squareSum(numbers){
+    //     let a = 0;
+    //     for (let i = 0; i < numbers.length; i++) {
+    //         a += numbers[i] * numbers[i]
+    //     }
+    //     return a;
+    // }
+
+// 2. Function Expression
+    // const squareSum = function (numbers) {
+    //     return numbers.reduce(function (a, b) {
+    //         return a + b * b
+    //     }, 0)
+    // }
+
+// 3. Arrow Function
+    // squareSum = number => number.reduce((a, b) => a + b * b, 0);
+
+// 4. Cara Lain 
+    const squareSum = numbers => numbers.reduce((a,b) => a + Math.pow(b, 2), 0)
+
+console.log(squareSum([1,2,2]));
