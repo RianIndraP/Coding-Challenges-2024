395a2cb33ba8616dd935d32c67e753b996974f14
RianIndraP <www.rian2008@gmail.com>
2024-11-18 13:12:47 +0700

    Update

--- 15-disemvowel.js
+++
@@ -1,9 +0,0 @@
-// WPU Challenge 2024
-// 15/366
-// https://www.codewars.com/kata/52fba66badcd10859f00097e/train/javascript
-
-function disemvowel(str) {
-  return str.replace(/[aiueoAIUEO]/g, '');
-}
-
-console.log(disemvowel("This website is for losers LOL!"));
